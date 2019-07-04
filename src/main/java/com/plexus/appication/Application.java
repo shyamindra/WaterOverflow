4240322e6ef7136782d245f0b103df679e357781
Shyam <shyam.dasari@sgdigital.com>
2019-07-05 09:56:49 +1000

    Updated input prompt

--- src/main/java/com/plexus/appication/Application.java
+++ src/main/java/com/plexus/appication/Application.java
@@ -11,8 +11,8 @@ public class Application {
         Scanner scanner = new Scanner(System.in);
         StringBuffer inp = getInput(scanner, "Enter the no of rows:");
         GlassStack glassStack = new GlassStack(Integer.parseInt(inp.toString()), 250);
-        inp = getInput(scanner, "Enter the volume to be filled:");
-        glassStack.fillLiquid(Integer.parseInt(inp.toString()));
+        inp = getInput(scanner, "Enter the volume to be filled in litres:");
+        glassStack.fillLiquid((int)(Double.parseDouble(inp.toString())*1000));
         inp = getInput(scanner, "Enter the row and column of the glass to find the volume (comma seperated):");
         int [] coordinates = Stream.of(inp.toString()
                 .split(",")).mapToInt(Integer::parseInt).toArray();
